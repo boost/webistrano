6fd3661addc14b3773b07fbda1829f69d62f7d37
Federico Gonzalez <fedegl@gmail.com>
2012-05-22 11:09:36 +1200

    Remove archiva file, we have added it to the Boost fork of capistrano

--- config/initializers/webistrano_configuration.rb
+++ config/initializers/webistrano_configuration.rb
@@ -5,12 +5,10 @@ end
 
 WEBISTRANO_VERSION = '1.5'
 
-ActionMailer::Base.delivery_method = WebistranoConfig[:smtp_delivery_method] 
-ActionMailer::Base.smtp_settings = WebistranoConfig[:smtp_settings] 
+ActionMailer::Base.delivery_method = WebistranoConfig[:smtp_delivery_method]
+ActionMailer::Base.smtp_settings = WebistranoConfig[:smtp_settings]
 
 Notification.webistrano_sender_address = WebistranoConfig[:webistrano_sender_address]
 
 ExceptionNotification::Notifier.exception_recipients = WebistranoConfig[:exception_recipients]
 ExceptionNotification::Notifier.sender_address = WebistranoConfig[:exception_sender_address]
-
-require "#{Rails.root}/lib/webistrano/deploy/scm/archiva"
